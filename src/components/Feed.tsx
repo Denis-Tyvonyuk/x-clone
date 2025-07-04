94029f4b3450e94cf314b9689ec934e61c3fdc66
Denis-Tyvonyuk <denusa321@gmail.com>
2025-07-04 18:36:21 +0300

    w

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,41 @@
+import { prisma } from "@/prisma";
 import Post from "./Post";
+import { auth } from "@clerk/nextjs/server";
 
-const Feed = () => {
+const Feed = async ({ userProfileId }: { userProfileId?: string }) => {
+  const { userId } = await auth();
+
+  if (!userId) return;
+  const following = await prisma.follow.findMany({
+    where: { followerId: userId },
+    select: { followingId: true },
+  });
+  const whereCondition = userProfileId
+    ? { parentPostId: null, userId: userProfileId }
+    : {
+        parentPostId: null,
+        userId: {
+          in: [
+            userId,
+            ...(
+              await prisma.follow.findMany({
+                where: { followerId: userId },
+                select: { followingId: true },
+              })
+            ).map((follow) => follow.followingId),
+          ],
+        },
+      };
+
+  const posts = await prisma.post.findMany({ where: whereCondition });
+
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Post />
+        </div>
+      ))}
     </div>
   );
 };
